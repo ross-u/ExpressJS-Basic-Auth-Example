fd10e4b3e1005d2d869de06f721c3bb4515d7fb6
Uros Cirkovic <cirkovic.uros.srb@gmail.com>
2019-11-21 11:53:31 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,71 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const mongoose = require('mongoose');
+
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+require('dotenv').config();
+
+var router = require('./routes/index');
+var siteRouter = require('./routes/site-routes');
+
+var app = express();
+
+mongoose.connect('mongodb://localhost:27017/basic-auth-demo', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Before the routes
+// SESSION ( & COOKIES ) MIDDLEWARE   -- req.session.currentUser
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    // cookie: { maxAge: 3600000 } // 1 hour
+    resave: true,
+    saveUninitialized: false,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 60 * 60 * 24 * 7, // Default - 14 days
+    }),
+  }),
+);
+
+
+
+// Routes
+app.use('/', router);  // '/' , '/login' '/auth' '/signup'
+app.use('/', siteRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+const authRouter = require('./auth');
+const loginRouter = require('./login');
+const logoutRouter = require('./logout');
+const signupRouter = require('./signup');
+
+// *  '/auth'
+router.use('/auth', authRouter);
+
+// *  '/login'
+router.use('/login', loginRouter);
+
+// *  '/logout'
+router.use('/logout', logoutRouter);
+
+// *  '/signup'
+router.use('/signup', signupRouter);
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Auth Demo' });
+});
+
+module.exports = router;
